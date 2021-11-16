ea004d6db0a38e7d036aa99c75bd41b534128bea
Selam <84750917+sel6@users.noreply.github.com>
2021-11-16 16:31:20 +0300

    score greater than 10

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,5 @@
+-- script that lists all records with a score >= 10 in the table second_table of the database hbtn_0c_0
+SELECT score, name
+FROM second_table
+ORDER BY score DESC,
+WHERE score >= 10;
