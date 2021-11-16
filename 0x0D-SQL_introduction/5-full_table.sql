b0a3c87ac1f8a46acf0b196ff7e321813a07d9eb
Selam <84750917+sel6@users.noreply.github.com>
2021-11-16 14:24:42 +0300

    show columns from table in db

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 --prints the full description of the table first_table from the database hbtn_0c_0 in MySQL server
-SHOW COLUMNS first_table FROM hbtn_0c_0;
+SHOW COLUMNS FROM first_table FROM hbtn_0c_0;
