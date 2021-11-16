7b3f796a336f7f3305d4ee813c15906b4719daec
Selam <84750917+sel6@users.noreply.github.com>
2021-11-16 21:55:36 +0300

    Update 12-no_cheating.sql

--- 0x0D-SQL_introduction/12-no_cheating.sql
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -1,5 +1,4 @@
--- cript that updates the score of Bob to 10 in the table second_table
-SELECT score, name
-FROM second_table
-UPDATE second_table SET score = '10' WHERE name = 'Bob'
-ORDER BY score DESC;
+-- script that updates the score of Bob to 10 in the table second_table
+UPDATE second_table
+SET score = '10'
+WHERE 'name' = 'Bob';
