b56c336cbfb2f5312b9f645c9e00938019ad101b
Selam <84750917+sel6@users.noreply.github.com>
2021-11-16 22:47:54 +0300

    Update 15-groups.sql

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,4 @@
 -- script that computes the score average of all records in the table second_table
-SELECT score, COUNT(score) FROM second_table GROUP BY HAVING COUNT(score) > 1
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
