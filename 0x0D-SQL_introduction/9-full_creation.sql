35e67251173764d0de44448e69d1184b92a909d8
Selam <84750917+sel6@users.noreply.github.com>
2021-11-16 15:59:41 +0300

    add score

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -4,5 +4,5 @@ id INT,
 name VARCHAR(256),
 score INT);
 INSERT INTO second_table (
-id, name)
+id, name, score)
 VALUES('1', 'John', '10'), ('2', 'Alex', '3'), ('3', 'Bob', '14'), ('4', 'George', '8');
