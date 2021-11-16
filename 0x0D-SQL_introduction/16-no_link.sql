5b24e0450a9fd1bf72af488b1d66e7e01a799e17
Selam <84750917+sel6@users.noreply.github.com>
2021-11-16 23:25:26 +0300

    Update 16-no_link.sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- script that lists all records of the table second_table
-SELECT (*) FROM second_table IF name EXISTS
-ORDER BY score DESC;
+SELECT score, name
+FROM second_table IF name EXISTS ORDER BY score DESC;
