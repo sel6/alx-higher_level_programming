152ccc8868e834f25c6a935b8e18a3edcb1faa0c
Selam <84750917+sel6@users.noreply.github.com>
2021-11-16 16:42:45 +0300

    Update 10-top_score.sql

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- script that lists all records of the table second_table
 SELECT score, name 
 FROM second_table
-ORDER BY score DESC;
-WHERE score >= '10'
+ORDER BY score DESC
+WHERE score >= '10';
