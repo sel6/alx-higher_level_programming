1f4f1947228c19fcfbe9bbc212fd62e9669ea8ce
Selam <84750917+sel6@users.noreply.github.com>
2021-11-16 14:37:44 +0300

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
 --prints the full description of the table first_table from the database hbtn_0c_0 in MySQL server
-SHOW CREATE TABLE hbtn_0c_0.first_table;
+SHOW CREATE TABLE 'hbtn_0c_0.first_table';
