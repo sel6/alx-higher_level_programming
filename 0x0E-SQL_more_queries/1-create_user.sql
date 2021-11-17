4416555d8c15d1753f4d1157efd22518c9d9d7b6
Selam <84750917+sel6@users.noreply.github.com>
2021-11-17 14:39:02 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,4 @@
 -- a script that creates the MySQL server user user_0d_1
 -- the two asterix represent table and data respectively
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANTS ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
