26351dde86dfb9ea7952aeb882cea5f9426afda5
Selam <84750917+sel6@users.noreply.github.com>
2021-11-17 09:23:47 +0300

    Update 1-create_user.sql

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,6 @@
 -- a script that creates the MySQL server user user_0d_1
 -- the two asterix represent table and data respectively
 -- then we should reload privelges
-CREATE USER 'user_0d_1'@'localhost IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
