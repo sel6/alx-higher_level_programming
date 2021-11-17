5efa7d1d28e19530093db07fe2b766d4ae86bb3e
Selam <84750917+sel6@users.noreply.github.com>
2021-11-17 14:46:51 +0300

    Update 5-unique_id.sql

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,2 @@
 --  a script that creates the table unique_id
-CREATE TABLE IF NOT EXISTS unique_id ( id INT DEFALUT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id ( id INT UNIQUE DEFAULT 1, name VARCHAR(256));
