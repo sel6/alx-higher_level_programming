f6c6cead9de786fdff662af213e24da54287d717
Selam <84750917+sel6@users.noreply.github.com>
2021-11-17 20:52:46 +0300

    after number 9

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,8 @@
---  that lists all shows contained in hbtn_0d_tvshows
-SELECT s.title, g.genre_id
-FROM tv_shows AS s
-LEFT JOIN tv_show_genres AS g
-ON s.id = g.show_id
-ORDER BY s.title, g.genre_id;
+-- Lists all shows in the database hbtn_0d_tvshows without a genre linked.
+
+SELECT s.`title`, g.`genre_id`
+FROM `tv_shows` AS s
+LEFT JOIN `tv_show_genres` AS g
+ON s.`id` = g.`show_id`
+WHERE g.`genre_id` IS NULL
+ORDER BY s.`title`, g.`genre_id`;
